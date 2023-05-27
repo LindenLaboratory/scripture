ccf8f50c470674b1462cf0ed0b0c56b1b9d7bb43
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2023-05-27 21:05:54 +0100

    Use this to train your Scripture model
    You can use this to train your model with `import train as t` and 't.trainList(questionlist,answerlist)' or just add your data to the data file with your own code

--- train.py
+++ train.py
@@ -1,12 +1,19 @@
 import json,csv
-def trainJson(tpath,rpath):
-    with open(tpath) as file:data = json.load(file)
-    questions,answers = [],[]
-    for paragraph in data['data'][0]['paragraphs']:
-        for qa in paragraph['qas']:question = qa['question'];answer = qa['answers'][0]['text'];questions.append(question);answers.append(answer)
-    with open(rpath, 'w', newline='') as file:writer = csv.writer(file);writer.writerow(['Question', 'Answer']);writer.writerows(zip(questions, answers))
-    print("Extraction completed and saved to data.csv")
+def trainJson(tpath, rpath):
+    with open(tpath) as file: data = json.load(file)
+    questions, answers = [], []
+    for section in data['data']:
+        title = section['title']
+        for paragraph in section['paragraphs']:
+            for qa in paragraph['qas']:
+                question = qa['question']
+                if qa['answers']: answer = qa['answers'][0]['text']
+                else:answer = ""
+                questions.append(question);answers.append(answer)
+    with open(rpath, 'w', newline='', encoding='utf-8') as file: writer = csv.writer(file);writer.writerow(['Question', 'Answer']);writer.writerows(zip(questions, answers))
+    print("Extraction completed and saved to", rpath)
 def trainList(questions, answers, output_file="data.csv"):
     with open(output_file, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file);writer.writerow(['Question', 'Answer'])
         for question, answer in zip(questions, answers): writer.writerow([question, answer])
+trainJson("data.json","data.csv")
